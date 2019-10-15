104cd5fa7fe400caec0e204c3a9f2874a4ac970c
单泠浩 <shanlh841271734@live.com>
2019-10-16 01:18:12 +0800

    :sparkles: 更新 demo

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,102 @@
 <template>
   <div id="app">
     <Tinder
-      v-cloak
       ref="tinder"
-      :allow-super="true"
       key-name="id"
+      :allow-super="true"
       :pointer-threshold="0.5"
       :super-threshold="0.5"
       :queue.sync="queue"
       @submit="submit"
+      :max="3"
+      :offset-y="10"
     >
+      <!-- :max="3"
+      :offset-y="10" -->
       <template slot-scope="scope">
         <div
           class="pic"
           :style="{
-            'background-image': `url(${scope.data.url})`
+            'background-image': `url(https://cn.bing.com//th?id=OHR.${
+              scope.data.id
+            }_UHD.jpg&pid=hp&w=720&h=1280&rs=1&c=4&r=0)`
           }"
-        ></div>
+        />
       </template>
       <img class="like-pointer" slot="like" src="~img/like-txt.png" />
       <img class="nope-pointer" slot="nope" src="~img/nope-txt.png" />
       <img class="super-pointer" slot="super" src="~img/super-txt.png" />
+      <img class="rewind-pointer" slot="rewind" src="~img/rewind-txt.png" />
     </Tinder>
     <div class="btns">
-      <img
-        style="width:70px;"
-        src="~img/super-like.png"
-        @click="decide('super')"
-      />
-      <img style="width:80px;" src="~img/nope.png" @click="decide('nope')" />
-      <img style="width:80px;" src="~img/like.png" @click="decide('like')" />
-      <img style="width:70px;" src="~img/help.png" @click="decide('help')" />
+      <img src="~img/rewind.png" @click="decide('rewind')" />
+      <img src="~img/nope.png" @click="decide('nope')" />
+      <img src="~img/super-like.png" @click="decide('super')" />
+      <img src="~img/like.png" @click="decide('like')" />
+      <img src="~img/help.png" @click="decide('help')" />
     </div>
   </div>
 </template>
 
 <script>
 import Tinder from '@/components/vue-tinder/Tinder.vue'
+import source from '@/bing'
+
 export default {
   name: 'App',
   components: { Tinder },
-  data: () => ({ queue: [] }),
+  data: () => ({
+    queue: [],
+    start: 0,
+    now: 0,
+    offset: 0,
+    history: []
+  }),
   created() {
-    this.getData()
+    this.mock()
   },
   methods: {
-    async getData() {
-      const res = await fetch(
-        'https://api.thecatapi.com/v1/images/search?limit=5'
-      )
-      const list = await res.json()
-      this.queue = this.queue.concat(list)
-    },
-    submit() {
-      // info
-      if (this.queue.length < 2) {
-        this.getData()
+    mock(count = 5, append = true) {
+      const list = []
+      for (let i = 0; i < count; i++) {
+        list.push({ id: source[this.offset], offset: this.offset })
+        this.offset += 1
+      }
+      if (append) {
+        this.queue = this.queue.concat(list)
+      } else {
+        this.queue.unshift(...list)
       }
     },
+    submit({ item }) {
+      if (this.queue.length < 3) {
+        this.mock()
+      }
+      this.history.push(item)
+    },
     async decide(choice) {
-      if (choice === 'help') {
-        const res = await fetch(
-          'https://api.thecatapi.com/v1/images/search?limit=1'
-        )
-        const list = await res.json()
-        this.queue.unshift(...list)
+      if (choice === 'rewind') {
+        if (this.history.length) {
+          //一个个 rewind
+          // this.$refs.tinder.rewind([this.history.pop()])
+          // 一次性 rewind 全部
+          // this.$refs.tinder.rewind(this.history)
+          // this.history = []
+          // 一次随机 rewind 多个
+          this.$refs.tinder.rewind(
+            this.history.splice(-Math.ceil(Math.random() * 3))
+          )
+          // 非 api调用的添加
+          // this.queue.unshift(this.history.pop())
+          // this.queue.push(this.history.pop())
+          // 非头部添加
+          // this.queue.splice(1, 0, this.history.pop())
+          // 一次性 rewind 多个，并且含有非头部添加的 item
+          // this.queue.unshift(this.history.pop())
+          // this.queue.unshift(...this.history)
+        }
+      } else if (choice === 'help') {
+        //
       } else {
         this.$refs.tinder.decide(choice)
       }
@@ -93,7 +125,7 @@ body {
   top: 23px;
   margin: auto;
   width: calc(100% - 20px);
-  height: calc(100% - 23px - 18%);
+  height: calc(100% - 23px - 65px - 47px - 16px);
   min-width: 300px;
   max-width: 355px;
 }
@@ -110,6 +142,7 @@ body {
 .nope-pointer {
   right: 10px;
 }
+
 .like-pointer {
   left: 10px;
 }
@@ -125,6 +158,15 @@ body {
   height: 78px;
 }
 
+.rewind-pointer {
+  position: absolute;
+  z-index: 1;
+  top: 20px;
+  right: 10px;
+  width: 112px;
+  height: 78px;
+}
+
 .pic {
   width: 100%;
   height: 100%;
@@ -136,13 +178,54 @@ body {
   position: absolute;
   left: 0;
   right: 0;
-  bottom: 0;
+  bottom: 47px;
   margin: auto;
-  height: 18%;
+  height: 65px;
   display: flex;
   align-items: center;
   justify-content: center;
   min-width: 300px;
   max-width: 355px;
 }
+
+.btns img {
+  margin-right: 12px;
+  box-shadow: 0 4px 9px rgba(0, 0, 0, 0.15);
+  border-radius: 50%;
+  cursor: pointer;
+  -webkit-tap-highlight-color: transparent;
+}
+
+.btns img:nth-child(2n + 1) {
+  width: 53px;
+}
+
+.btns img:nth-child(2n) {
+  width: 65px;
+}
+
+.btns img:nth-last-child(1) {
+  margin-right: 0;
+}
+
+/* .vue-tinder.right-end,
+.vue-tinder.left-end {
+  transform: translateZ(20px);
+}
+.vue-tinder.right-end .tinder-card:nth-child(1) {
+  animation: rightEnd 0.2s ease-in-out;
+}
+.vue-tinder.left-end .tinder-card:nth-child(1) {
+  animation: leftEnd 0.2s ease-in-out;
+}
+@keyframes leftEnd {
+  50% {
+    transform: rotateY(8deg);
+  }
+}
+@keyframes rightEnd {
+  50% {
+    transform: rotateY(-8deg);
+  }
+} */
 </style>
