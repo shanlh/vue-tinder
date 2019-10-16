cbd51a66e6f56e8d5f9c1ea5d3bf8072b4ee5cd4
单泠浩 <shanlh841271734@live.com>
2019-10-16 23:51:36 +0800

    :sparkles: 支持使用 vue.use 加载

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
 import Tinder from './vue-tinder/Tinder.vue'
 
+Tinder.install = Vue => {
+  Vue.component('vue-tinder', Tinder)
+}
+
 export default Tinder
 
 if (typeof window !== 'undefined' && window.Vue) {
