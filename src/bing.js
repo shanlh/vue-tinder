72e8b07eeee1832b1e4cb811fc8b89ea88c84ff5
单泠浩 <shanlh841271734@live.com>
2019-10-17 13:40:04 +0800

    :clown_face: 更新示例图

--- src/bing.js
+++ src/bing.js
@@ -1,4 +1,8 @@
 export default [
+  'AdelieBreeding_ZH-CN1750945258',
+  'BarcolanaTrieste_ZH-CN5745744257',
+  'RedRocksArches_ZH-CN5664546697',
+  'NationalDay70_ZH-CN1636316274',
   'LofotenSurfing_ZH-CN5901239545',
   'UgandaGorilla_ZH-CN5826117482',
   'FeatherSerpent_ZH-CN5706017355',
